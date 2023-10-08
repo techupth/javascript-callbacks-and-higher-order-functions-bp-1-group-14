82129941bf274586f41d6ca5388e6b780d1ef4fb
Parot Ketpong <parot@mtrgroup.local>
2023-10-08 16:40:33 +0700

    Fixed : Exercise Callback and Higher

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,20 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
+function LeastFive(studentscores){
+  let result = 0;
+  for(let i = 0; i < studentscores.length; i++){
+    if (studentscores[i] >= 70){
+      result =result +1
+    }
+}if(result>=5){
+  return "True"
+}else{
+  return "False"
+}
+}
+function atLeastFive(array,Operation) {
+// Start coding here\
+return Operation(array)
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +23,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result1 = atLeastFive(studentScoresRoom1,LeastFive);
+console.log(scoreRoom1Result1)
+let scoreRoom2Result2 = atLeastFive(studentScoresRoom2,LeastFive);
+console.log(scoreRoom2Result2)
+let scoreRoom3Result3= atLeastFive(studentScoresRoom3,LeastFive);
+console.log(scoreRoom3Result3)
